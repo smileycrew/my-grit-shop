266099958d7a097b76a400f29fd0acbf2cfd10d5
Edwin Moz <edwin.moz@icloud.com>
2024-07-25 16:25:18 -0500

    fixed children type

--- src/components/form-button.tsx
+++ src/components/form-button.tsx
@@ -1,5 +1,9 @@
 import { Button } from "./ui/button"
 
-export default function FormButton({ children }) {
+export default function FormButton({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return <Button>{children}</Button>
 }
